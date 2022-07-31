7624a33356bfe9c95fbd82eff8029d4805e7f78e
Enrique Zarate <ezarate@Enriques-MacBook-Pro.local>
2022-07-31 14:27:47 -0700

    Create app API

--- app.js
+++ app.js
@@ -3,10 +3,13 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+require('./app_api/models/db');
 
 var indexRouter = require('./app_server/routes/index');
 var usersRouter = require('./app_server/routes/users');
 var travelRouter = require('./app_server/routes/travel');
+var apiRouter = require('./app_api/routes/index');
+
 const { hasSubscribers } = require('diagnostics_channel');
 const hbs = require('hbs');
 
@@ -28,6 +31,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/travel', travelRouter);
+app.use('/api', apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
